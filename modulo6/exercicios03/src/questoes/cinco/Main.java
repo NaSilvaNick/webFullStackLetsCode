d5bc3e342f887dcbaa3e7a8dd2ca503d898f8727
Nickolas Silva <nicklops8@gmail.com>
2021-12-11 18:34:57 -0300

    Quinta questão finalizada

---
+++ modulo6/exercicios03/src/questoes/cinco/Main.java
@@ -0,0 +1,44 @@
+package questoes.cinco;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Lista de convidados");
+
+        String[] listaConvidados = criaListaConvidados(5);
+
+        String maiorNome = obtemMaiorNome(listaConvidados);
+
+        imprimeMaiorNome(maiorNome);
+    }
+
+    private static String[] criaListaConvidados(int tamanho){
+        Scanner Leitor = new Scanner(System.in);
+        String[] lista = new String[tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("Digite o " + (i + 1) + "° nome: ");
+            lista[i] = Leitor.next();
+        }
+
+        return lista;
+    }
+
+    private static String obtemMaiorNome(String[] lista){
+        String maiorNome = "";
+        for (String nome : lista) {
+            if (nome.length() > maiorNome.length()){
+                maiorNome = nome;
+            }
+
+        }
+        return maiorNome;
+    }
+
+    private static void imprimeMaiorNome(String maiorNome){
+        System.out.println("O maior nome é o : " + maiorNome);
+    }
+}
